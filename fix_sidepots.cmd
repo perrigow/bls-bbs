5706c23244ebda37d94d28e45b43fe8e7b50a7ba
Wayne Perrigo <83511562+perrigow@users.noreply.github.com>
2022-10-11 23:07:39 -0400

    updated script and added feedback

--- fix_sidepots.cmd
+++ fix_sidepots.cmd
@@ -7,32 +7,34 @@ set regulars=Regulars.txt
 set csvfile=Sidepots.csv
 
 :: Delete CSV file if exists
-if EXIST %csvfile% (
-  del /F %csvfile%
+if exist %csvfile% (
+  echo Deleting CSV file...
+  del /f %csvfile%
 )
 
 :: Display message and exit if Regulars.txt is missing
-if NOT EXIST %regulars% (
+if not exist %regulars% (
   echo %regulars% does not exist
   pause
   exit 1
 )
 
 :: Loop over text file, set defaults for subs, remove average bk and write final CSV
-for /F "usebackq tokens=1-5 delims=," %%a in ("%txtfile%") do (
+echo Processing TXT file...
+for /f "usebackq tokens=1-5 delims=," %%a in ("%txtfile%") do (
   set team=%%b
   set nwlane=%%e
-  findstr /I /R /C:%%a %regulars% > nul
-  if !ERRORLEVEL! NEQ 0 (
+  findstr /i /r /c:%%a %regulars% > nul
+  if !ERRORLEVEL! neq 0 (
     set team="0"
     set nwlane=""
   )
   set avg=%%d
-  rem echo %%a,!team!,%%c,!avg:bk=!,!nwlane!
   echo %%a,!team!,%%c,!avg:bk=!,!nwlane!>>%csvfile%
 )
 
 :: Delete TXT file if exists
-if EXIST %txtfile% (
-  del /F %txtfile%
+if exist %txtfile% (
+  echo Deleting TXT file
+  del /f %txtfile%
 )
